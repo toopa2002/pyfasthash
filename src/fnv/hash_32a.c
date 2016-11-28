9ea60a68bf1a1d69c284524a19203812eabf39b5
Vichan Chutitaporn <vichan.chutitaporn@thomsonreuters.com>
2016-11-28 15:54:05 +0700

    fix

--- src/fnv/hash_32a.c
+++ src/fnv/hash_32a.c
@@ -93,11 +93,7 @@ fnv_32a_buf(void *buf, size_t len, Fnv32_t hval)
 	hval ^= (Fnv32_t)*bp++;
 
 	/* multiply by the 32 bit FNV magic prime mod 2^32 */
-#if defined(NO_FNV_GCC_OPTIMIZATION)
 	hval *= FNV_32_PRIME;
-#else
-	hval += (hval<<1) + (hval<<4) + (hval<<7) + (hval<<8) + (hval<<24);
-#endif
     }
 
     /* return our new hash value */
@@ -132,11 +128,7 @@ fnv_32a_str(char *str, Fnv32_t hval)
 	hval ^= (Fnv32_t)*s++;
 
 	/* multiply by the 32 bit FNV magic prime mod 2^32 */
-#if defined(NO_FNV_GCC_OPTIMIZATION)
 	hval *= FNV_32_PRIME;
-#else
-	hval += (hval<<1) + (hval<<4) + (hval<<7) + (hval<<8) + (hval<<24);
-#endif
     }
 
     /* return our new hash value */
